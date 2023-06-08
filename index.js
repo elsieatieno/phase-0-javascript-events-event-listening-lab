ef7eeb31b0803dac6de59907762d43492bbbee6b
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-06-08 22:12:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+input = document.getElementById('button');
+sinon.spy(input, 'addEventListener');
+
 function addingEventListener() {
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => {
+        addingEventListener();
+    });
 }
+
